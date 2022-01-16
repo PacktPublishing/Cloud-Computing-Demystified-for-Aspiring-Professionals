18435c2b0b4433aab2e528b6f0104df03141e729
DavidS <david3651@live.com>
2022-01-16 18:27:40 -0500

    all files, instructions, chapter 1, chapter 2, and image files added

---
+++ Allfiles/Chapter2/dockerFileExample/dockerFileExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dockerFileExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
